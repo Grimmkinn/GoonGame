e85b5b4ba1ca9db9270ccb80e376e067d20caec7
Nova <thezxn0va@gmail.com>
2026-08-30 12:58:24 +0200

    movement2?

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,29 +2,26 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
     private float horizontal;
     private float vertical;
     private float speed = 8f;
-    private bool isFacingUp = true;
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
-
-    // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        Flip();
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
-        rb.linearVelocity = new Vector2(vertical * speed, rb.linearVelocity.x);
+        Vector2 moveInput = new Vector2(horizontal, vertical).normalized;
+        rb.linearVelocity = moveInput * speed;
     }
 
     private void Flip()
@@ -34,15 +31,7 @@ public class Movement : MonoBehaviour
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
-            transform.localScale = localScale
-        }
-
-        if (isFacingUp && vertical < 0f || !isFacingUp && vertical > 0f)
-        {
-            isFacingUp = !isFacingUp;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale
+            transform.localScale = localScale;
         }
     }
 }
