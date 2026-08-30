fcb72fa140b4961e5308ef13fa8a70eee43277c6
Nova <thezxn0va@gmail.com>
2026-08-30 13:30:04 +0200

    movemnt 4

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,39 +2,33 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private float horizontal;
-    private float vertical;
-    private float speed = 8f;
-    private bool isFacingRight = true;
+    private float speed = 0.5f;
+    private Rigidbody2D rb;
+    private Vector2 input;
 
-    private Vector2 lastMoveDirection = Vector2.down;
 
-    [SerializeField] private Rigidbody2D rb;
 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-        Flip();
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        input.Normalize();
     }
 
     private void FixedUpdate()
     {
-        Vector2 moveInput = new Vector2(horizontal, vertical).normalized;
-        rb.linearVelocity = moveInput * speed;
+        rb.linearVelocity = input * speed;
 
-        if (moveInput != Vector2.zero)
-            lastMoveDirection = moveInput;
     }
 
     private void Flip()
     {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
+
     }
 }
