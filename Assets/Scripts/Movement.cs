96dfbbf3bbfa384481442cfe5e9a4a42b7b61b05
Nova <thezxn0va@gmail.com>
2026-08-30 15:07:26 +0200

    Moabawhvdba

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -8,20 +9,47 @@ public class Movement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveInput;
 
+=======
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    private float speed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 Input;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+>>>>>>> Stashed changes
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+<<<<<<< Updated upstream
     void Update()
     {
         rb.linearVelocity = moveInput * moveSpeed;
+=======
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = Input * speed;
+>>>>>>> Stashed changes
     }
 
     public void Move(InputAction.CallbackContext context)
     {
+<<<<<<< Updated upstream
         moveInput = context.ReadValue<Vector2>();
     }
     
 }
 
 
+=======
+        Input = context.ReadValue<Vector2>();
+    }
+}
+>>>>>>> Stashed changes
