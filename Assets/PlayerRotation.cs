2816b0bd4792ac6251e2f6c7597a08d4346d4fb7
Grimmkinn <grimmkin3011@gmail.com>
2026-09-02 22:14:42 +0200

    bam

---
+++ Assets/PlayerRotation.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Assets
+{
+    public class PlayerRotation
+    {
+
+        private void Update()
+        {
+            LookAt(Vector3.zero);
+        }
+    }
+}
